d634852ea6aa837b3401c170fd041075ab9bd1ef
Wito <w.budzioch@beliveo.pl>
2018-12-14 21:48:45 +0100

    New features 1: Add simple validation for username field.

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import $ from 'cash-dom';
 export class App {
   initializeApp() {
     $('.load-username').on('click', () => {
+      $('.username.input').removeClass('is-danger');
       const userName = $('.username.input').val();
-
-      fetch('https://api.github.com/users/' + userName)
+      const isUserNameValid = (/^[a-zA-Z0-9\-\_]+$/).test(userName);
+      if (isUserNameValid) {
+        fetch('https://api.github.com/users/' + userName)
         .then((response) => {
           if (!!response.ok) {
             return response.json();
@@ -20,13 +22,16 @@ export class App {
         .catch((errorPromise) => errorPromise.then((error) => {
           alert(error.message);
         }));
+      } else {
+        $('.username.input').addClass('is-danger');
+      }
     });
   }
 
   update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+    $('#profile-name').text(this.profile.name);
+    $('#profile-image').attr('src', this.profile.avatar_url);
+    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login);
+    $('#profile-bio').text(this.profile.bio || '(no information)');
   }
 }
