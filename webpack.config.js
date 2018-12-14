c468f397f308c9251ae2e286e0d954ba62d8f1da
Wito <w.budzioch@beliveo.pl>
2018-12-14 19:57:14 +0100

    Updated packages, webpack configuration, moved babel-loader options into .babelrc, added browserslist for postcss autoprefixer.

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,129 @@
-const path = require('path'),
-  webpack = require('webpack'),
-  CleanWebpackPlugin = require('clean-webpack-plugin'),
-  HtmlWebpackPlugin = require('html-webpack-plugin'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const extractPlugin = new ExtractTextPlugin({filename: './assets/css/app.css'});
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 const config = {
-
-  context: path.resolve(__dirname, 'src'),
-
-  entry: {
-    app: './index.js'
-  },
-
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: './assets/js/[name].bundle.js'
-  },
-
-  module: {
-    rules: [
-
-      {
-        test: /\.js$/,
-        include: /src/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['env']
-          }
-        }
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      },
-      {
-        test: /\.s?css$/,
-        use: extractPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            }, {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ],
-          fallback: 'style-loader'
-        })
-      },
-      {
-        test: /\.(jpg|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: './assets/media/'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      }
-
-    ]
-  },
-
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({template: 'index.html'}),
-    extractPlugin
-  ],
-
-  devServer: {
-    contentBase: path.resolve(__dirname, "./dist/assets/media"),
-    compress: true,
-    port: 2000,
-    stats: 'errors-only',
-    open: true
-  },
-
-  devtool: 'inline-source-map'
-
+	'context': path.resolve(__dirname, 'src'),
+	'devServer': {
+		'contentBase': path.resolve(__dirname, './dist/assets/media'),
+		'compress': true,
+		'port': 2000,
+		'stats': 'errors-only',
+		'open': true
+	},
+	'devtool': 'inline-source-map',
+	'plugins': [
+		new MiniCssExtractPlugin({
+			'filename': 'css/style.css',
+			'chunkFilename': 'css/[name].css'
+		}),
+		new HtmlWebpackPlugin({
+			'template': 'index.html',
+			'filename': 'index.html'
+		}),
+		new CleanWebpackPlugin(['dist'])
+	],
+	'entry': {
+		'app': './index.js'
+	},
+	'output': {
+		'path': path.resolve(__dirname, 'dist'),
+		'filename': 'js/[name].bundle.js'
+	},
+	'module': {
+		'rules': [
+			{
+				'test': /\.js$/,
+				'include': /src/,
+				'exclude': /node_modules/,
+				'use': [
+					{
+						'loader': 'babel-loader'
+					}
+				]
+			},
+			{
+				'test': /\.html$/,
+				'use': [
+					{
+						'loader': 'html-loader',
+						'options': {
+							'minimalize': true
+						}
+					}
+				]
+			},
+			{
+				'test': /\.s?css$/,
+				'exclude': /node_modules/,
+				'use': [
+					{
+						'loader': MiniCssExtractPlugin.loader
+					},
+					{
+						'loader': 'css-loader',
+						'options': {
+							'importLoaders': 2,
+							'sourceMap': true
+						}
+					},
+					{
+						'loader': 'postcss-loader',
+						'options': {
+							'ident': 'postcss',
+							'plugins': () => [
+								require('autoprefixer')()
+							]
+						}
+					},
+					{
+						'loader': 'sass-loader',
+						'options': {
+							'sourceMap': true
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpg|png|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: './assets/media/'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: ['file-loader']
+			}
+		]
+	},
+	'optimization': {
+		'namedModules': true,
+		'splitChunks': {
+			'cacheGroups': {
+				'default': false,
+				'commons': {
+					'test': /[\\/]node_modules[\\/].*\.js$/,
+					'name': 'vendor',
+					'chunks': 'all'
+				},
+				'style': {
+					'test': /\.(sass|scss|css)$/,
+					'name': 'style', 
+					'minChunks': 1,
+					'reuseExistingChunk': true,
+					'enforce': true
+				}
+			}
+		}
+	}
 };
 
 module.exports = config;
